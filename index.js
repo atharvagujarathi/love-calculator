d9ce1dad67659f953b5391f9c24b67753f3f25e8
atharvagujarathi <75681555+atharvagujarathi@users.noreply.github.com>
2022-01-16 19:02:53 +0530

    love calculator

---
+++ index.js
@@ -0,0 +1,11 @@
+var x = prompt("enter the 1st name");
+var y = prompt("enter the 2nd name");
+var z = Math.random();
+z = z*100;
+z = Math.floor(z) + 1;
+var t= alert("the love percentage of  " + x + " and " +  y + " is " +  z);
+if(z < 20){
+    alert("fake love bcc");
+}else {
+    alert("love each other");
+}
