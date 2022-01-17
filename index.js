477367b7a7e1a3f759a5712e38e13e7f7cbdfdad
atharvagujarathi <75681555+atharvagujarathi@users.noreply.github.com>
2022-01-17 12:27:28 +0530

    leap year

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ z = z*100;
 z = Math.floor(z) + 1;
 var t= alert("the love percentage of  " + x + " and " +  y + " is " +  z);
 if(z < 20){
-    alert("fake love bcc");
+    alert("fake love");
 }else {
     alert("love each other");
 }
---
+++ index1.js
@@ -0,0 +1,8 @@
+a = prompt("enter the year");
+if( a % 4 == 0 ){
+    console.log("its a leap year");
+}if( a % 100 && a % 400 == 0 ){
+    console.log("its not a leap year");
+}else{
+    console.log("its not a leap year");
+}
